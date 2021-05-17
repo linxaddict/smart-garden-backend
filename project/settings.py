b8477bacbcce979bb81eaca6134188f5a0c9f4a5
mprzepiorkowski <mprzepiorkowski@gmail.com>
2021-05-17 23:43:57 +0200

    integrated django-environ

--- project/settings.py
+++ project/settings.py
@@ -9,8 +9,13 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
 from pathlib import Path
+
+import environ
+
+root = environ.Path(__file__) - 3
+env = environ.Env()
+environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -19,12 +24,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-efqwglx6iazaw7&rtjmeemd3y$f)@oi)90z6rj@@yr-&5^%cs@'
+SECRET_KEY = env.str('SECRET_KEY', default='development_key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env.bool('DEBUG', default=False)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['app', 'localhost', '.elasticbeanstalk.com']
 AUTH_USER_MODEL = 'smartgarden.User'
 
 # Application definition
