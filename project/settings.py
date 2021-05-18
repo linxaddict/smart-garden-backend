3a2460b869a260f1064856ebf8cf3bb8794340cc
mprzepiorkowski <mprzepiorkowski@gmail.com>
2021-05-18 23:10:43 +0200

    switched to postgresql

--- project/settings.py
+++ project/settings.py
@@ -96,10 +96,7 @@ INTERNAL_IPS = [
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': env.db('DATABASE_URL')
 }
 
 # Password validation
