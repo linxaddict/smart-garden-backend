b80cf401f95151b6ebe075e1b2194b2dc706d03e
mprzepiorkowski <mprzepiorkowski@gmail.com>
2021-05-21 22:32:35 +0200

    integrated jwt authentication

--- project/settings.py
+++ project/settings.py
@@ -63,7 +63,7 @@ MIDDLEWARE = [
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.SessionAuthentication',
-        'rest_framework.authentication.TokenAuthentication'
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
     ],
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated'
--- project/urls.py
+++ project/urls.py
@@ -16,11 +16,13 @@ Including another URLconf
 import debug_toolbar
 from django.contrib import admin
 from django.urls import path, include
-from rest_framework.authtoken import views
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView
 
 urlpatterns = [
     path('', include('smartgarden.urls')),
     path('admin/', admin.site.urls),
-    path('api/auth/token', views.obtain_auth_token),
+    path('api/auth/token', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/auth/token/refresh', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/auth/token/verify', TokenVerifyView.as_view(), name='token_verify'),
     path('__debug__/', include(debug_toolbar.urls)),
 ]
